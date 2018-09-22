08d90f452e24970768065a1ac2751f376a688f60
pzync <shkhrgrv@gmail.com>
2018-09-22 11:48:11 +0530

    introduced the 'Like' component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
   state = {
@@ -8,6 +9,14 @@ class Movies extends Component {
 
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movie };
+    movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
 
@@ -27,6 +36,7 @@ class Movies extends Component {
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
               <th />
+              <th />
             </tr>
           </thead>
           <tbody>
@@ -37,6 +47,12 @@ class Movies extends Component {
                   <td>{movie.genre.name}</td>
                   <td>{movie.numberInStock}</td>
                   <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={movie.liked}
+                      onClick={() => this.handleLike(movie)}
+                    />
+                  </td>
                   <td>
                     <button
                       onClick={() => this.handleDelete(movie)}
